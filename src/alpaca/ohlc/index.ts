1ddc90cea83ade185524cb8aa539cd4ddb138850
boriswilliams <boriswilliams@live.co.uk>
2025-09-14 13:26:21 +0100

    crypto bars

--- src/alpaca/ohlc/index.ts
+++ src/alpaca/ohlc/index.ts
@@ -3,13 +3,14 @@
 // import type { paths } from '../../../types/alpaca/data';
 // import { headers } from '../headers';
 // import { baseUrl } from '../urls/data';
-import { getBars } from './utils';
+import { getCryptoBars, getStockBars } from './utils';
 
 export async function run() {
 
   // const client = createClient<paths>({ baseUrl });
 
-  const bars = await getBars('aapl', '1T', '2024-01-04T09:30:00', '2024-01-05T16:00:00');
+  // const bars = await getStockBars('aapl', '1D', '2024-01-04T09:30:00', '2025-09-14T16:00:00');
+  const bars = await getCryptoBars('BTC/USD', '1D', '2024-01-04T09:30:00', '2025-09-14T16:00:00');
 
-  console.log(bars.length);
+  console.log(bars);
 }
--- src/alpaca/ohlc/utils.ts
+++ src/alpaca/ohlc/utils.ts
@@ -4,12 +4,13 @@ import { DateTime } from 'luxon';
 import type { paths, operations, components } from '../../../types/alpaca/data';
 import { headers } from '../headers';
 import { baseUrl } from '../urls/data';
+import { EXCHANGE } from '@src/values';
 
 export function date(localET: string) {
   return DateTime.fromISO(localET, { zone: 'America/New_York' }).toUTC().toISO({ suppressMilliseconds: true }) ?? undefined;
 }
 
-export async function getBars(
+export async function getStockBars(
   ticker: string,
   timeframe: components["parameters"]["Timeframe"],
   startET: string,
@@ -18,15 +19,13 @@ export async function getBars(
   
   const client = createClient<paths>({ baseUrl });
 
-  const bars: components['schemas']['StockBar'][] = [];
-
   const query: operations['StockBars']['parameters']['query'] = {
     symbols: ticker.toLowerCase(),
     timeframe,
     start: date(startET),
     end: date(endET),
     adjustment: 'raw',
-    feed: 'sip',
+    feed: EXCHANGE,
     sort: 'asc'
   };
 
@@ -40,8 +39,58 @@ export async function getBars(
     }
   };
 
+  const bars: components['schemas']['StockBar'][] = [];
+
   while (await (async () => {
     const { data } = await client.GET('/v2/stocks/bars', options);
+
+    bars.push(...(data?.bars[ticker.toUpperCase()] ?? []));
+    
+    if (!data?.next_page_token) return false;
+    
+    query.page_token = data?.next_page_token;
+    return true;
+  })());
+
+  return bars;
+}
+
+export async function getCryptoBars(
+  ticker: string,
+  timeframe: components["parameters"]["Timeframe"],
+  startET: string,
+  endET: string
+): Promise<components['schemas']['CryptoBar'][]> {
+  
+  const client = createClient<paths>({ baseUrl });
+
+  const path: operations['CryptoBars']['parameters']['path'] = {
+    loc: 'us'
+  };
+
+  const query: operations['CryptoBars']['parameters']['query'] = {
+    symbols: ticker.toLowerCase(),
+    timeframe,
+    start: date(startET),
+    end: date(endET),
+    sort: 'asc'
+  };
+
+  const options = {
+    headers: {
+      ...headers,
+      accept: 'application/json'
+    },
+    params: {
+      path,
+      query
+    }
+  };
+
+  const bars: components['schemas']['CryptoBar'][] = [];
+
+  while (await (async () => {
+    const { data } = await client.GET('/v1beta3/crypto/{loc}/bars', options);
 
     bars.push(...(data?.bars[ticker.toUpperCase()] ?? []));
     
